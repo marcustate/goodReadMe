4e6900471dee3230292a5b71292b56e0c2e0beab
marcustate <58437889+marcustate@users.noreply.github.com>
2020-06-04 11:13:06 -0500

    updated api routes

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,27 @@
-const api = {
-  getUser(username) {
+const axios = require("acios");
+require("dotenv").config();
 
-  }
+
+const api = {
+	getUser(username) {
+		// Gets user information
+		const queryUrl = `https://api.github.com/users/${username}`;
+		console.log(queryUrl);
+		return axios.get(queryUrl).catch(err => {
+			console.log("User not found");
+			process.exit(1);
+		});
+	},
+	getStars(username) {
+		// Gets repo information for user
+		const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+		return axios.get(queryUrl).then(res => {
+			return res.data.reduce((acc, current) => {
+				acc += current.stargazers_count;
+				return acc;
+			}, 0);
+		});
+	}
 };
 
 module.exports = api;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+# ## Project Title
+${answers.projectTitle}
+## Project Description
+${answers.Description}
+## Table of Contents:
+[Installation](#installation)
+[Usage](#usage)
+[Credits](#credits)
+[License](#license)
+## Installation
+## Usage
+[Provide instructions and examples to use.]
+## Credits
+[List your collaborators.]
+## License
+${answers.License}
+## Badges
+${answers.totalStars}
+## GitHub Email
+${answers.email}
+## GitHub Username
+${answers.gitUsername}
+## Avatar URL
+![avatarurl](${answers.avatar_url})
 
 `;
 }
